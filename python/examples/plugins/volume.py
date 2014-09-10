56b60309201a443cd40fe5afdede45f8135cc40e
Phil Howard <Phil@gadgetoid.com>
2014-09-10 21:14:56 +0000

    Tweaked Volume plugin to track actual system volume

--- python/examples/plugins/volume.py
+++ python/examples/plugins/volume.py
@@ -4,16 +4,24 @@ class Volume(MenuOption):
 
   def __init__(self):
     self.volume = None
+    self.actual_volume = 0
+    self.last_update = 0
 
   def setup(self, lcd, config):
     MenuOption.setup(self, lcd, config)
     self.volume = int(self.get_option('Sound','volume',80))
     self.set_volume()
+    self.actual_volume = self.get_volume()
 
+  def get_volume(self):
+    actual_volume = subprocess.check_output("amixer get 'PCM' | awk '$0~/%/{print $4}' | tr -d '[]%'", shell=True)
+    return actual_volume.strip()
+   
   def set_volume(self):
     self.set_option('Sound','volume',self.volume)
     devnull = open(os.devnull, 'w')
     subprocess.call(['/usr/bin/amixer','sset',"'PCM'", str(self.volume) + '%'], stdout=devnull)
+    self.actual_volume = self.get_volume()
 
   def down(self):
     self.volume -= 1
@@ -28,12 +36,18 @@ class Volume(MenuOption):
     self.set_volume()
 
   def redraw(self):
-    lcd.write('Volume')
-    lcd.set_cursor_position(0,1)
-    lcd.write('PCM: ')
-    lcd.write(str(self.volume))
-    lcd.write('%')
+    self.lcd.write('Volume: ')
+    self.lcd.write(str(self.actual_volume))
+    self.lcd.write('%')
+    self.lcd.set_cursor_position(0,1)
+    self.lcd.write('Target: ')
+    self.lcd.write(str(self.volume))
+    self.lcd.write('%')
 
-    vol = '#' * int(16.0 * self.volume/100.0)
-    lcd.set_cursor_position(0,2)
-    lcd.write(vol)
+    vol = '#' * int(16.0 * float(self.actual_volume)/100.0)
+    self.lcd.set_cursor_position(0,2)
+    self.lcd.write(vol)
+
+    if self.millis() - self.last_update > 1000:
+      self.actual_volume = self.get_volume()
+      self.last_update = self.millis()
