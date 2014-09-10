5a9456e62397f695dbf8e9bff36139c607cc4cdd
Phil Howard <Phil@gadgetoid.com>
2014-09-10 10:47:39 +0000

    refactored and moved dot3k.menu into module

---
+++ python/dot3k/menu.py
@@ -0,0 +1,396 @@
+import time, ConfigParser, os, colorsys, atexit
+
+class Menu():
+  """
+  This class accepts a list of menu items,
+  Each key corresponds to a text item displayed on the menu
+  Each value can either be:
+  * A nested list, for a sub-menu
+  * A function, which is called immediately on select
+  * A class derived from MenuOption, providing interactive functionality
+  """
+
+  def __init__(self, structure, lcd):
+    self.list_location = []
+    self.lcd = lcd
+    self.current_position = 0
+    self.mode = 'navigate'
+    self.menu_options = structure
+
+    self.config = ConfigParser.ConfigParser()
+    self.config.read(['dot3k.cfg', os.path.expanduser('~/.dot3k.cfg')])
+
+    self.setup_menu(self.menu_options)
+
+    atexit.register(self.save)
+
+  def save(self):
+    with open('dot3k.cfg', 'wb') as configfile:
+      self.config.write(configfile)
+      print('Config saved to dot3k.cfg')
+
+  def setup_menu(self, menu):
+    for key in menu:
+      value = menu[key]
+      if type(value) is dict:
+        self.setup_menu(value)
+      elif isinstance(value,MenuOption):
+        value.setup(self.lcd,self.config)
+
+  def current_submenu(self):
+    """
+    Traverse the list of indexes in list_location
+    and find the relevant nested dictionary
+    """
+    menu = self.menu_options
+    for location in self.list_location:
+      menu = menu[menu.keys()[location]]
+    return menu
+
+  def current_value(self):
+    return self.current_submenu()[self.current_key()]
+    
+  def current_key(self):
+    """
+    Convert the integer current_position into
+    a valid key for the currently selected dictionary
+    """
+    return self.current_submenu().keys()[self.current_position]
+
+  def next_position(self):
+    position = self.current_position + 1
+    if position >= len(self.current_submenu()):
+      position = 0
+    return position
+
+  def previous_position(self):
+    position = self.current_position - 1
+    if position < 0:
+      position = len(self.current_submenu()) - 1
+    return position
+
+  def select_option(self):
+    """
+    Navigate into, or handle selected menu option accordingly
+    """
+    if type(self.current_value()) is dict:
+      self.list_location.append( self.current_position )
+      self.current_position = 0
+    elif isinstance(self.current_value(),MenuOption):
+      self.mode = 'adjust'
+    elif callable(self.current_submenu()[self.current_key()]):
+      self.current_submenu()[self.current_key()]()
+
+  def prev_option(self):
+    """
+    Decrement the option pointer,
+    select previous menu item
+    """
+    self.current_position = self.previous_position()
+
+  def next_option(self):
+    """
+    Increment the option pointer,
+    select next menu item
+    """
+    self.current_position = self.next_position()
+
+  def exit_option(self):
+    """
+    Exit current submenu and restore position
+    in previous menu
+    """
+    if len(self.list_location) > 0:
+      self.current_position = self.list_location.pop()
+
+  def select(self):
+    """
+    Handle "select" action
+    """
+    if self.mode == 'navigate':
+      self.select_option()
+    elif self.mode == 'adjust':
+      # The "select" call must return true to exit the adjust
+      if self.current_value().select():
+        self.mode = 'navigate'
+    #self.redraw()
+
+  def up(self):
+    if self.mode == 'navigate':
+      self.prev_option()
+    elif self.mode == 'adjust':
+      self.current_value().up()
+    #self.redraw()
+
+  def down(self):
+    if self.mode == 'navigate':
+      self.next_option()
+    elif self.mode == 'adjust':
+      self.current_value().down()
+    #self.redraw()
+
+  def left(self):
+    if self.mode == 'navigate':
+      self.exit_option()
+    elif self.mode == 'adjust':
+      if not self.current_value().left():
+        self.mode = 'navigate'
+    #self.redraw()
+
+  def right(self):
+    if self.mode == 'navigate':
+      self.select_option()
+    elif self.mode == 'adjust':
+      self.current_value().right()
+    #self.redraw()
+
+  def redraw(self):
+    self.lcd.clear()
+    if self.mode == 'navigate':
+      self.lcd.set_cursor_position(1,1) # x, y
+      self.lcd.write(chr(252))
+      self.lcd.write(self.current_submenu().keys()[self.current_position])
+
+      if len(self.current_submenu()) > 2:
+        self.lcd.set_cursor_position(1,0)
+        self.lcd.write(self.current_submenu().keys()[self.previous_position()])
+
+      if len(self.current_submenu()) > 1:
+        self.lcd.set_cursor_position(1,2)
+        self.lcd.write(self.current_submenu().keys()[self.next_position()])
+
+    elif self.mode == 'adjust':
+      self.current_value().redraw()
+
+class MenuOption():
+  def __init__(self):
+    self.lcd = None
+    self.config = None
+  def millis(self):
+    return int(round(time.time() * 1000))
+  def up(self):
+    pass
+  def down(self):
+    pass
+  def left(self):
+    pass
+  def right(self):
+    pass
+  def select(self):
+    # Must return true to allow exit
+    return True
+  def redraw(self, lcd):
+    pass
+  def setup(self, lcd, config):
+    self.lcd = lcd
+    self.config = config
+
+  def set_option(self, section, option, value):
+    if self.config != None:
+      if not section in self.config.sections():
+        self.config.add_section(section)
+      self.config.set(section, option, value)
+
+  def get_option(self, section, option, default = None):
+    if not section in self.config.sections():
+      self.config.add_section(section)
+    if option in self.config.options(section):
+      return self.config.get(section, option)
+    elif default == None:
+      return False
+    else:
+      self.config.set(section, option, str(default))
+      return default
+
+class Backlight(MenuOption):
+  def __init__(self, backlight):
+    self.backlight = backlight
+    self.hue = 0
+    self.sat = 100
+    self.val = 100
+    self.mode = 0
+    self.modes = ['h','s','v','r','g','b']
+    self.from_hue()
+    MenuOption.__init__(self)
+
+  def from_hue(self):
+    rgb = colorsys.hsv_to_rgb(self.hue,self.sat/100.0,self.val/100.0)
+    self.r = int(255*rgb[0])
+    self.g = int(255*rgb[1])
+    self.b = int(255*rgb[2])
+
+  def from_rgb(self):
+    self.hue = colorsys.rgb_to_hsv(self.r/255.0,self.g/255.0,self.b/255.0)[0]
+
+  def setup(self, lcd, config):
+    self.lcd = lcd
+    self.config = config
+
+    self.r = int(self.get_option('Backlight','r',255))
+    self.g = int(self.get_option('Backlight','g',255))
+    self.b = int(self.get_option('Backlight','b',255))
+
+    self.hue = float(self.get_option('Backlight','h',0)) / 359.0
+    self.sat = int(self.get_option('Backlight','s',0))
+    self.val = int(self.get_option('Backlight','v',100))
+
+    self.backlight.rgb(self.r,self.g,self.b)
+
+  def update_bl(self):
+    self.set_option('Backlight','r',str(self.r))
+    self.set_option('Backlight','g',str(self.g))
+    self.set_option('Backlight','b',str(self.b))
+    self.set_option('Backlight','h',str(int(self.hue*359)))
+    self.set_option('Backlight','s',str(self.sat))
+    self.set_option('Backlight','v',str(self.val))
+
+    self.backlight.rgb(self.r,self.g,self.b)
+
+  def right(self):
+    self.mode+=1
+    if self.mode >= len(self.modes):
+     self.mode = 0
+    return True
+
+  def left(self):
+    self.mode-=1
+    if self.mode < 0:
+     self.mode = len(self.modes)-1
+    return True
+
+  def up(self):
+    if self.mode == 0:
+      self.hue += (1.0/359.0)
+      if self.hue > 1:
+        self.hue = 0
+      self.from_hue()
+
+    elif self.mode == 1: # sat
+      self.sat += 1
+      if self.sat > 100:
+        self.sat = 0
+        self.from_hue()
+
+    elif self.mode == 2: # val
+      self.val += 1
+      if self.val > 100:
+        self.val = 0
+      self.from_hue()
+
+    else: # rgb
+      if self.mode == 3: #r
+        self.r += 1
+        if self.r > 255:
+          self.r = 0
+      elif self.mode == 4: #g
+        self.g += 1
+        if self.g > 255:
+          self.g = 0
+      elif self.mode == 5: #b
+        self.b += 1
+        if self.b > 255:
+          self.b = 0
+
+      self.from_rgb()
+     
+    self.update_bl()
+    return True
+
+  def down(self):    
+    if self.mode == 0:
+      self.hue -= (1.0/359.0)
+      if self.hue < 0:
+        self.hue = 1
+      self.from_hue()
+
+    elif self.mode == 1: # sat
+      self.sat -= 1
+      if self.sat < 0:
+        self.sat = 100
+      self.from_hue()
+
+    elif self.mode == 2: #val
+      self.val -= 1
+      if self.val < 0:
+        self.val = 100
+      self.from_hue()
+
+    else: # rgb
+      if self.mode == 3: #r
+        self.r -= 1
+        if self.r < 0:
+          self.r = 255
+      elif self.mode == 4: #g
+        self.g -= 1
+        if self.g < 0:
+          self.g = 255
+      elif self.mode == 5: #b
+        self.b -= 1
+        if self.b < 0:
+          self.b = 255
+
+      self.from_rgb()
+
+    self.update_bl()
+    return True
+
+  def redraw(self):
+    self.lcd.set_cursor_position(0,0)
+    self.lcd.write('Backlight')
+    self.lcd.set_cursor_position(0,1)
+    self.lcd.write('HSV: ' + str(int(self.hue*359)).zfill(3) + ' ' + str(self.sat).zfill(3) + ' ' + str(self.val).zfill(3) )
+    self.lcd.set_cursor_position(0,2) 
+
+    self.lcd.write('RGB: ' + str(self.r).zfill(3) + ' ' + str(self.g).zfill(3) + ' ' + str(self.b).zfill(3))
+
+    # Position the cursor next to the value we're modifying
+    if self.mode == 0: # hue
+      self.lcd.set_cursor_position(4,1)
+    elif self.mode == 1: # sat
+      self.lcd.set_cursor_position(8,1)
+    elif self.mode == 2: # val
+      self.lcd.set_cursor_position(12,1)
+    elif self.mode == 3: # r
+      self.lcd.set_cursor_position(4,2)
+    elif self.mode == 4: # g
+      self.lcd.set_cursor_position(8,2)
+    elif self.mode == 5: # b
+      self.lcd.set_cursor_position(12,2)
+
+    # Write the little arrow!
+    self.lcd.write(chr(252))
+ 
+class Contrast(MenuOption):
+  def __init__(self):
+    self.contrast = 30
+    MenuOption.__init__(self)
+
+  def right(self):
+    self.contrast+=1
+    if self.contrast > 63:
+      self.contrast = 0
+    self.update_contrast()
+    return True
+
+  def left(self):
+    self.contrast-=1
+    if self.contrast < 0:
+      self.contrast = 63
+    self.update_contrast()
+    return True
+
+  def setup(self, lcd, config):
+    self.lcd = lcd
+    self.config = config
+    self.contrast = int(self.get_option('Display','contrast',40))
+    self.lcd.set_contrast(self.contrast)
+
+  def update_contrast(self):
+    self.set_option('Display','contrast',str(self.contrast))
+    self.lcd.set_contrast(self.contrast)
+
+  def redraw(self):
+    self.lcd.set_cursor_position(1,0)
+    self.lcd.write('Contrast')
+    self.lcd.set_cursor_position(1,1)
+    self.lcd.write('Value: ' + str(self.contrast))
--- python/examples/menu.py
+++ python/examples/menu.py
@@ -1,358 +1,21 @@
 #!/usr/bin/env python
 
-import dot3k.joystick as j
-import dot3k.lcd as l
-import dot3k.backlight as b
-import signal, time, ConfigParser, os
+import dot3k.joystick as joystick
+import dot3k.lcd as lcd
+import dot3k.backlight as backlight
+from dot3k.menu import Menu, Backlight, Contrast, MenuOption
+import time, os, math, psutil
 
-config = ConfigParser.ConfigParser()
-config.read(['dot3k.cfg', os.path.expanduser('~/.dot3k.cfg')])
 
-class Menu():
-
-  def __init__(self, structure):
-    self.list_location = []
-    self.current_position = 0
-    self.mode = 'navigate'
-    self.menu_options = structure
-    #self.redraw()
-    #l.create_char(0,[16,24,28,30,30,28,24,16])
-    
-    @j.on(j.UP)
-    def handle_up(pin):
-      self.up()
-      j.repeat(j.UP,self.up,0.4,0.9)
-
-    @j.on(j.DOWN)
-    def handle_down(pin):
-      self.down()
-      j.repeat(j.DOWN,self.down,0.4,0.9)
-
-    @j.on(j.LEFT)
-    def handle_left(pin):
-      self.left()
-      j.repeat(j.LEFT,self.left,0.4,0.9)
-
-    @j.on(j.RIGHT)
-    def handle_right(pin):
-      self.right()
-      j.repeat(j.RIGHT,self.right,0.4,0.9)
-
-    @j.on(j.BUTTON)
-    def handle_button(pin):
-      self.select()
-
-  def current_submenu(self):
-    """
-    Traverse the list of indexes in list_location
-    and find the relevant nested dictionary
-    """
-    menu = self.menu_options
-    for location in self.list_location:
-      menu = menu[menu.keys()[location]]
-    return menu
-
-  def current_value(self):
-    return self.current_submenu()[self.current_key()]
-    
-  def current_key(self):
-    """
-    Convert the integer current_position into
-    a valid key for the currently selected dictionary
-    """
-    return self.current_submenu().keys()[self.current_position]
-
-  def next_position(self):
-    position = self.current_position + 1
-    if position >= len(self.current_submenu()):
-      position = 0
-    return position
-
-  def previous_position(self):
-    position = self.current_position - 1
-    if position < 0:
-      position = len(self.current_submenu()) - 1
-    return position
-
-  def select_option(self):
-    """
-    Navigate into, or handle selected menu option accordingly
-    """
-    if type(self.current_value()) is dict:
-      self.list_location.append( self.current_position )
-      self.current_position = 0
-    elif isinstance(self.current_value(),MenuOption):
-      self.mode = 'adjust'
-    elif callable(self.current_submenu()[self.current_key()]):
-      self.current_submenu()[self.current_key()]()
-
-  def prev_option(self):
-    """
-    Decrement the option pointer,
-    select previous menu item
-    """
-    self.current_position = self.previous_position()
-
-  def next_option(self):
-    """
-    Increment the option pointer,
-    select next menu item
-    """
-    self.current_position = self.next_position()
-
-  def exit_option(self):
-    """
-    Exit current submenu and restore position
-    in previous menu
-    """
-    if len(self.list_location) > 0:
-      self.current_position = self.list_location.pop()
-
-  def select(self):
-    """
-    Handle "select" action
-    """
-    if self.mode == 'navigate':
-      self.select_option()
-    elif self.mode == 'adjust':
-      # The "select" call must return true to exit the adjust
-      if self.current_value().select():
-        self.mode = 'navigate'
-    #self.redraw()
-
-  def up(self):
-    if self.mode == 'navigate':
-      self.prev_option()
-    elif self.mode == 'adjust':
-      self.current_value().up()
-    #self.redraw()
-
-  def down(self):
-    if self.mode == 'navigate':
-      self.next_option()
-    elif self.mode == 'adjust':
-      self.current_value().down()
-    #self.redraw()
-
-  def left(self):
-    if self.mode == 'navigate':
-      self.exit_option()
-    elif self.mode == 'adjust':
-      if not self.current_value().left():
-        self.mode = 'navigate'
-    #self.redraw()
-
-  def right(self):
-    if self.mode == 'navigate':
-      self.select_option()
-    elif self.mode == 'adjust':
-      self.current_value().right()
-    #self.redraw()
-
-  def redraw(self):
-    l.clear()
-    if self.mode == 'navigate':
-      l.set_cursor_position(1,1) # x, y
-      l.write(chr(252))
-      l.write(self.current_submenu().keys()[self.current_position])
-
-      if len(self.current_submenu()) > 2:
-        l.set_cursor_position(1,0)
-        l.write(self.current_submenu().keys()[self.previous_position()])
-
-      if len(self.current_submenu()) > 1:
-        l.set_cursor_position(1,2)
-        l.write(self.current_submenu().keys()[self.next_position()])
-
-    elif self.mode == 'adjust':
-      self.current_value().redraw()
-
-class MenuOption():
-   def millis(self):
-     return int(round(time.time() * 1000))
-   def up(self):
-     pass
-   def down(self):
-     pass
-   def left(self):
-     pass
-   def right(self):
-     pass
-   def select(self):
-     # Must return true to allow exit
-     return True
-   def redraw(self, lcd):
-     pass
-
-import colorsys
-class Backlight(MenuOption):
-   def __init__(self):
-     self.hue = 0
-     self.sat = 100
-     self.val = 100
-     self.mode = 0
-     self.modes = ['h','s','v','r','g','b']
-     self.from_hue()
-
-   def from_hue(self):
-     rgb = colorsys.hsv_to_rgb(self.hue,self.sat/100.0,self.val/100.0)
-     self.r = int(255*rgb[0])
-     self.g = int(255*rgb[1])
-     self.b = int(255*rgb[2])
- 
-   def from_rgb(self):
-     self.hue = colorsys.rgb_to_hsv(self.r/255.0,self.g/255.0,self.b/255.0)[0]
-
-   def update_bl(self):
-     if not 'Backlight' in config.sections():
-       config.add_section('Backlight')
-     config.set('Backlight','r',str(self.r))
-     config.set('Backlight','g',str(self.g))
-     config.set('Backlight','b',str(self.b))
-     config.set('Backlight','h',str(int(self.hue*359)))
-     config.set('Backlight','s',str(self.sat))
-     config.set('Backlight','v',str(self.val))
-     b.rgb(self.r,self.g,self.b)
-
-   def right(self):
-     self.mode+=1
-     if self.mode >= len(self.modes):
-       self.mode = 0
-     return True
-
-   def left(self):
-     self.mode-=1
-     if self.mode < 0:
-       self.mode = len(self.modes)-1
-     return True
-
-   def up(self):
-     if self.mode == 0:
-       self.hue += (1.0/359.0)
-       if self.hue > 1:
-         self.hue = 0
-       self.from_hue()
-
-     elif self.mode == 1: # sat
-       self.sat += 1
-       if self.sat > 100:
-         self.sat = 0
-         self.from_hue()
- 
-     elif self.mode == 2: # val
-       self.val += 1
-       if self.val > 100:
-         self.val = 0
-       self.from_hue()
-
-     else: # rgb
-       if self.mode == 3: #r
-         self.r += 1
-         if self.r > 255:
-           self.r = 0
-       elif self.mode == 4: #g
-         self.g += 1
-         if self.g > 255:
-           self.g = 0
-       elif self.mode == 5: #b
-         self.b += 1
-         if self.b > 255:
-           self.b = 0
-   
-       self.from_rgb()
-       
-     self.update_bl()
-     return True
-
-   def down(self):    
-     if self.mode == 0:
-       self.hue -= (1.0/359.0)
-       if self.hue < 0:
-         self.hue = 1
-       self.from_hue()
-
-     elif self.mode == 1: # sat
-       self.sat -= 1
-       if self.sat < 0:
-         self.sat = 100
-       self.from_hue()
-
-     elif self.mode == 2: #val
-       self.val -= 1
-       if self.val < 0:
-         self.val = 100
-       self.from_hue()
-
-     else: # rgb
-       if self.mode == 3: #r
-         self.r -= 1
-         if self.r < 0:
-           self.r = 255
-       elif self.mode == 4: #g
-         self.g -= 1
-         if self.g < 0:
-           self.g = 255
-       elif self.mode == 5: #b
-         self.b -= 1
-         if self.b < 0:
-           self.b = 255
-
-       self.from_rgb()
-
-     self.update_bl()
-     return True
-   
-   def redraw(self):
-     l.set_cursor_position(0,0)
-     l.write('Backlight')
-     l.set_cursor_position(0,1)
-     l.write('HSV: ' + str(int(self.hue*359)).zfill(3) + ' ' + str(self.sat).zfill(3) + ' ' + str(self.val).zfill(3) )
-     l.set_cursor_position(0,2) 
-
-     l.write('RGB: ' + str(self.r).zfill(3) + ' ' + str(self.g).zfill(3) + ' ' + str(self.b).zfill(3))
-     if self.mode == 0: # hue
-       l.set_cursor_position(4,1)
-     elif self.mode == 1: # sat
-       l.set_cursor_position(8,1)
-     elif self.mode == 2: # val
-       l.set_cursor_position(12,1)
-     elif self.mode == 3: # r
-       l.set_cursor_position(4,2)
-     elif self.mode == 4: # g
-       l.set_cursor_position(8,2)
-     elif self.mode == 5: # b
-       l.set_cursor_position(12,2)
-     l.write(chr(252))
- 
-class Contrast(MenuOption):
-   def __init__(self):
-     self.contrast = 30
-
-   def right(self):
-     self.contrast+=1
-     if self.contrast > 63:
-       self.contrast = 0
-     l.set_contrast(self.contrast)
-     return True
-
-   def left(self):
-     self.contrast-=1
-     if self.contrast < 0:
-       self.contrast = 63
-     l.set_contrast(self.contrast)
-     return True
-   
-   def redraw(self):
-     l.set_cursor_position(1,0)
-     l.write('Contrast')
-     l.set_cursor_position(1,1)
-     l.write('Value: ' + str(self.contrast))
-
-import math, psutil 
 class GraphCPU(MenuOption):
+  """
+  A simple "plug-in" example, this gets the CPU load
+  and draws it to the LCD when active
+  """
   def __init__(self):
     self.cpu_samples = [0,0,0,0,0]
     self.last = self.millis()
+    MenuOption.__init__(self)
   
   def redraw(self):
     now = self.millis()
@@ -362,15 +25,20 @@ class GraphCPU(MenuOption):
     self.cpu_samples.append(psutil.cpu_percent() / 100.0)
     self.cpu_samples.pop(0)
     self.cpu_avg = sum(self.cpu_samples) / len(self.cpu_samples)
-    l.set_cursor_position(1,0)
-    l.write('CPU Load')
-    l.set_cursor_position(1,1)
-    l.write(str(self.cpu_avg) + '%')
 
-import commands
+    self.lcd.set_cursor_position(1,0)
+    self.lcd.write('CPU Load')
+    self.lcd.set_cursor_position(1,1)
+    self.lcd.write(str(self.cpu_avg) + '%')
+
 class GraphTemp(MenuOption):
+  """
+  A simple "plug-in" example, this gets the Temperature
+  and draws it to the LCD when active
+  """
   def __init__(self):
     self.last = self.millis()
+    MenuOption.__init__(self)
 
   def get_cpu_temp(self):
     tempFile = open( "/sys/class/thermal/thermal_zone0/temp" )
@@ -387,17 +55,12 @@ class GraphTemp(MenuOption):
     if now - self.last < 1000:
       return false
 
-    l.set_cursor_position(1,0)
-    l.write('Temperature')
-    l.set_cursor_position(1,1)
-    l.write('CPU:' + str(self.get_cpu_temp()))
-    l.set_cursor_position(1,2)
-    l.write('GPU:' + str(self.get_gpu_temp()))
-
-def save_settings():
-  print('Config saved to dot3k.cfg')
-  with open('dot3k.cfg', 'wb') as configfile:
-    config.write(configfile)
+    self.lcd.set_cursor_position(1,0)
+    self.lcd.write('Temperature')
+    self.lcd.set_cursor_position(1,1)
+    self.lcd.write('CPU:' + str(self.get_cpu_temp()))
+    self.lcd.set_cursor_position(1,2)
+    self.lcd.write('GPU:' + str(self.get_gpu_temp()))
 
 
 """
@@ -409,27 +72,48 @@ be used as menu items to show information or change settings.
 
 See GraphTemp, GraphCPU, Contrast and Backlight for examples.
 """
-my_menu = {
-  'Status': {
-    'CPU':GraphCPU(),
-    'Temp':GraphTemp()
-  },
-  'Settings': {
-    'Display': {
-      'Contrast':Contrast(),
-      'Backlight':Backlight()
+menu = Menu({
+    'Status': {
+      'CPU':GraphCPU(),
+      'Temp':GraphTemp()
     },
-    'Save':save_settings
-  }
-}
+    'Settings': {
+      'Display': {
+        'Contrast':Contrast(),
+        'Backlight':Backlight(backlight)
+      }
+    }
+  },
+  lcd)
 
-l.set_contrast(40)
-b.rgb(255,255,255)
-menu = Menu(my_menu)
+"""
+You can use anything to control dot3k.menu,
+but you'll probably want to use dot3k.joystick
+"""
+@joystick.on(joystick.UP)
+def handle_up(pin):
+  menu.up()
+  joystick.repeat(joystick.UP,menu.up,0.4,0.9)
+
+@joystick.on(joystick.DOWN)
+def handle_down(pin):
+  menu.down()
+  joystick.repeat(joystick.DOWN,menu.down,0.4,0.9)
+
+@joystick.on(joystick.LEFT)
+def handle_left(pin):
+  menu.left()
+  joystick.repeat(joystick.LEFT,menu.left,0.4,0.9)
+
+@joystick.on(joystick.RIGHT)
+def handle_right(pin):
+  menu.right()
+  joystick.repeat(joystick.RIGHT,menu.right,0.4,0.9)
+
+@joystick.on(joystick.BUTTON)
+def handle_button(pin):
+  menu.select()
 
 while 1:
   menu.redraw()
   time.sleep(0.05)
- 
-# Prevent the script exiting!
-signal.pause()
