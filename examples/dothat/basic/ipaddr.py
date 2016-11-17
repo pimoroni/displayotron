7bc9544c459394b757e87adcb9924b2343fa0109
RogueM <chaosgen6@googlemail.com>
2016-11-17 01:10:54 +0000

    basic ip retrieval example

---
+++ examples/dothat/basic/ipaddr.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import fcntl
+import socket
+import struct
+
+import dothat.lcd as lcd
+
+
+def get_addr(ifname):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        return socket.inet_ntoa(fcntl.ioctl(
+            s.fileno(),
+            0x8915,  # SIOCGIFADDR
+            struct.pack('256s', ifname[:15].encode('utf-8'))
+        )[20:24])
+    except IOError:
+        return 'Not Found!'
+
+wlan0 = get_addr('wlan0')
+eth0 = get_addr('eth0')
+
+lcd.clear()
+
+if wlan0 != 'Not Found!':
+    lcd.set_cursor_position(0, 1)
+    lcd.write(wlan0)
+else:
+    lcd.write('wlan0 {}'.format(wlan0))
+
+if eth0 != 'Not Found!':
+    lcd.set_cursor_position(1, 1)
+    lcd.write(eth0)
+else:
+    lcd.write('eth0 {}'.format(eth0))
