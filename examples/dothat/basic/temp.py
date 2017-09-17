27d0db759c8cc77b11c07318e3d80ec0000e7727
imjoseangel <josea.munoz@gmail.com>
2017-09-17 15:20:08 +0000

    Moving Graph from 0 to 1 and viceversa

--- examples/dothat/basic/temp.py
+++ examples/dothat/basic/temp.py
@@ -3,22 +3,22 @@
 from dothat import lcd
 from dothat import backlight
 import time
+import math
 
 lcd.set_contrast(50)
+x = 0
 
 while True:
+    x += 1
     tempC = int(open('/sys/class/thermal/thermal_zone0/temp').read()) / 1e3
     
     # Change backlight if temp changes
     if tempC < 60:
         backlight.rgb(0,255,0)
-        backlight.set_graph(0.4)
     elif tempC > 70:
         backlight.rgb(255,0,0)
-        backlight.set_graph(0.8)
     else:
         backlight.rgb(0,255,255)
-        backlight.set_graph(0.6)
 
     # Convert Temp to String
     tempF = str(tempC)
@@ -26,5 +26,6 @@ while True:
     # Write Temp and wait 1 sec.
     lcd.set_cursor_position(0, 0)
     lcd.write("Temp: " +  tempF + " C")
+    backlight.set_graph(abs(math.sin(x / 100.0)))
     time.sleep(1)
     lcd.clear()
